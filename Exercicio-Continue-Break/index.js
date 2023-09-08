0b895300a8bb0b15212b0f60f9fd3341b954d882
kleber-galvao <klebergalvao3@hotmail.com>
2023-09-08 16:05:10 -0300

    envio do exercicio de Continue e Break

---
+++ Exercicio-Continue-Break/index.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let number of numbers) {
+
+    if (number === 2 || number === 5) {
+        continue;
+    }
+
+    if (number === 7) {
+        break;
+    }
+
+    console.log(number);
+}
