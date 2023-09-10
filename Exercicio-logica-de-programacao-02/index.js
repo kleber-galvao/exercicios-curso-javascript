1d91e0f42673f67ce69dd69a55481b965d31c336
kleber-galvao <klebergalvao3@hotmail.com>
2023-09-10 10:03:13 -0300

    resolucao do exercicio de logica de programacao 02

---
+++ Exercicio-logica-de-programacao-02/index.js
@@ -0,0 +1,11 @@
+// function ePaisagem(largura, altura) {
+//     // if (largura > altura) {
+//     //     return true;
+//     // } return false;
+//     return largura > altura;
+// } 
+
+
+const ePaisagem = (largura, altura) => largura > altura; // arrow function
+
+console.log(ePaisagem(1920, 1080));
