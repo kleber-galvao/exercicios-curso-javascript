3368036ca782ed61845210c4776cb16419c37d15
kleber-galvao <klebergalvao3@hotmail.com>
2023-09-11 18:01:33 -0300

    resolução do exercicio timer com setInterval

---
+++ Exercicio-Timer-com-setInterval/index.js
@@ -0,0 +1,40 @@
+function criaHoraSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () { //soma de 1 em 1 segundo na variavel segundos
+        segundos++;
+        relogio.innerHTML = criaHoraSegundos(segundos); // deixa formatado no formato horario no timer
+    }, 1000);
+};
+
+iniciar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer); // para contagem
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function (event) {
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+
+zerar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
+
